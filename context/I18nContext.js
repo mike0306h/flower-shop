d48705701f7d1004019d3ed0f702e60edab98d2e
Mike <bkkflowershOP@gmail.com>
2026-04-21 19:01:54 +0700

    fix: add missing useEffect import in I18nContext

--- context/I18nContext.js
+++ context/I18nContext.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 
 const I18nContext = createContext()
 
